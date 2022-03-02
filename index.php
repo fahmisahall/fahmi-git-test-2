6d58bb614af3bbd6ecaa1d3f613e7a3d93d2bf1a
fahmimsahal <221910796@stis.ac.id>
2022-03-02 21:20:12 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, asperiores! Dolor blanditiis, quas tempore consequuntur commodi eveniet quasi natus non? Ab officia, dicta ducimus suscipit quisquam minima repellendus. Perspiciatis, quibusdam.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
